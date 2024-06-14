aeeded5b713520abedb3256728fb153d08091872
abahgo <123719425+abahgo@users.noreply.github.com>
2024-06-14 22:32:38 +0100

    Add files via upload

---
+++ Assignment.py
@@ -0,0 +1,11 @@
+information= "my first python assignment"
+print(information)
+first_name="Gideon:"
+last_name="Abah:"
+Email_address="abahgo@gmail.com:"
+password="greatness."
+print(first_name+last_name+Email_address+password)
+
+input(first_name)
+input(last_name)
+print(input+input)
